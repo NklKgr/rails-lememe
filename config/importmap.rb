6c043c2f3f62c07317c2a9d2efc3161fb313e98e
Nkl Kgr <krueger.lup@web.de>
2023-06-09 12:31:12 +0200

    fixes

--- config/importmap.rb
+++ config/importmap.rb
@@ -7,3 +7,4 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "bootstrap", to: "https://ga.jspm.io/npm:bootstrap@5.3.0/dist/js/bootstrap.esm.js"
 pin "@popperjs/core", to: "https://unpkg.com/@popperjs/core@2.11.2/dist/esm/index.js"
+pin "canvas/canvas_generator"
