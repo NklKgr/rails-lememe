ac421378a7d6e57f2596a7a2132a392c161e01e9
Gilad Milashut <gilad.milashut@gmail.com>
2023-06-12 16:36:32 +0200

    working_upvote_feature

--- app/javascript/controllers/votings_controller.js
+++ app/javascript/controllers/votings_controller.js
@@ -2,20 +2,56 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="voting"
 export default class extends Controller {
-  static targets = ["upvotes"]
+  static targets = ["upvotes", "upvoteBtn", "downvoteBtn" ]
 
   connect() {
     // console.log("Hello, Stimulus!")
   }
 
   addVoteClass(event) {
-    // event.preventDefault();
-    event.currentTarget.classList.add('clicked-vote');
+    if (event.currentTarget === this.upvoteBtnTarget && this.downvoteBtnTarget.classList.contains('clicked-vote')) {
+      this.downvoteBtnTarget.classList.remove('clicked-vote');
+
+    } else if (event.currentTarget === this.downvoteBtnTarget && this.upvoteBtnTarget.classList.contains('clicked-vote')) {
+      this.upvoteBtnTarget.classList.remove('clicked-vote');
+    }
+
+    if (event.currentTarget.classList.contains('clicked-vote')) {
+      event.currentTarget.classList.remove('clicked-vote');
+      this.upvotesTarget.innerText = parseInt(this.upvotesTarget.innerText) - 1
+    } else {
+      event.currentTarget.classList.add('clicked-vote');
+      this.upvotesTarget.innerText = parseInt(this.upvotesTarget.innerText) + 1
+    }
+  }
+
+  addVoteClassDown(event) {
+    // if (event.currentTarget.classList.contains('clicked-vote')) {
+    //   event.currentTarget.classList.remove('clicked-vote');
+    //   this.upvotesTarget.innerText = parseInt(this.upvotesTarget.innerText) + 1
+    // } else {
+    //   event.currentTarget.classList.add('clicked-vote');
+    //   this.upvotesTarget.innerText = parseInt(this.upvotesTarget.innerText) - 1
+    // }
+    if (event.currentTarget === this.downvoteBtnTarget && this.upvoteBtnTarget.classList.contains('clicked-vote')) {
+      this.upvoteBtnTarget.classList.remove('clicked-vote');
+
+    } else if (event.currentTarget === this.upvoteBtnTarget && this.downvoteBtnTarget.classList.contains('clicked-vote')) {
+      this.upvoteBtnTarget.classList.remove('clicked-vote');
+    }
+
+    if (event.currentTarget.classList.contains('clicked-vote')) {
+      event.currentTarget.classList.remove('clicked-vote');
+      this.upvotesTarget.innerText = parseInt(this.upvotesTarget.innerText) + 1
+    } else {
+      event.currentTarget.classList.add('clicked-vote');
+      this.upvotesTarget.innerText = parseInt(this.upvotesTarget.innerText) - 1
+    }
   }
 
   upvote(event) {
-    // console.log("upvote")
     event.preventDefault();
+    console.log("upvote")
     const id = this.data.get("id")
     const url = `/memes/${id}/upvote`
     fetch(url, { method: "PATCH" })
